a560fbe2a6175b25f1f228c7c4a94915097e2aec
Taber McFarlin <tabermcfarlin@gmail.com>
2018-12-12 16:26:18 -0500

    Dependencies and basic landing page

---
+++ client/src/components/App.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App__header">
+          <h1 className="App__header__title">Moth Balls</h1>
+        </header>
+        <p className="App__intro">
+          Moth ball's trainging thing. It'll get you ripped, bro.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
