293722cf45b39b358a0706e95e00b1c9e89e6520
Matt Rota <mrota@stevens.edu>
2018-12-12 15:56:39 -0500

    added user creation

--- backend/built/models/user.js
+++ backend/built/models/user.js
@@ -1,9 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var bcrypt_1 = require("bcrypt");
-var user_salt = 10; // User salt
-var user_schema = new mongoose_1.Schema({
+import { model, Schema } from 'mongoose';
+let user_salt = 10; // User salt
+const user_schema = new Schema({
     username: {
         type: String,
         unique: true,
@@ -17,22 +14,5 @@ var user_schema = new mongoose_1.Schema({
     bio: String,
     images: [String]
 });
-user_schema.pre('save', function (next) {
-    var _this = this;
-    if (this.isModified('password') || this.isNew) {
-        bcrypt_1.genSalt(user_salt, function (err, salt) {
-            if (err)
-                return next(err);
-            bcrypt_1.hash(_this.password, salt, null, function (err, hash) {
-                if (err)
-                    return next(err);
-                _this.password = hash;
-                next();
-            });
-        });
-    }
-    else
-        return next();
-});
-var UserModel = mongoose_1.model('User', user_schema);
-exports.default = UserModel;
+var User = model('User', user_schema);
+module.exports = User;
