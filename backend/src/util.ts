71e672cdae4b6620d9dd890ac20cc4d0bd383910
Ben Iofel <ben@flow.io>
2018-12-13 14:34:05 -0500

    fix style

--- backend/src/util.ts
+++ backend/src/util.ts
@@ -2,6 +2,6 @@ import { Request, Response } from 'express';
 
 export function handleErrors(fn: (req: Request, res: Response) => Promise<void>) {
     return (req, res, next) => {
-        fn(req, res).catch(next)
+        fn(req, res).catch(next);
     };
 }
