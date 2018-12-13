21be0d6aac0a266995e10063d004655f51d76144
Ben Iofel <ben@flow.io>
2018-12-13 16:39:46 -0500

    client: format code

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,21 +1,18 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
-export const Navbar: React.StatelessComponent<{}> = () => {
-    return (
-        <nav id="navbar">
-            <a href="#">Home</a>
+export const Navbar = () =>
+  <nav id="navbar">
+    <a href="#">Home</a>
 
-            <div>
-                <ul>
-                    <li>
-                        <Link to="/account/login">Login</Link>
-                    </li>
-                    <li>
-                        <Link to="/account/register">Register</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    )
-}
+    <div>
+      <ul>
+        <li>
+          <Link to="/account/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/account/register">Register</Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
