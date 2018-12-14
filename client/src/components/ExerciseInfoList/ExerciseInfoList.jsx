8635afe40d1e1f622905d60cba7fa7421d418e47
Taber McFarlin <tabermcfarlin@gmail.com>
2018-12-14 07:43:35 -0500

    Convert ExerciseInfoList to functional component

--- client/src/components/ExerciseInfoList/ExerciseInfoList.jsx
+++ client/src/components/ExerciseInfoList/ExerciseInfoList.jsx
@@ -1,38 +1,30 @@
 import React from 'react';
 import ExerciseInfo from './ExerciseInfo';
 
-class ExerciseInfoList extends React.Component {
-  constructor({ exerciseList }) {
-    super({ exerciseList });
-  }
+const ExerciseInfoList = ({ exerciseList }) => {
+  const anchorMap = exerciseList.map(exercise => (
+    <li key={`Anchor${exercise._id}`}>
+      <a href={`#${exercise.name.replace(/\s/g, '')}`}>{exercise.name}</a>
+    </li>
+  ));
 
-  render() {
-    const { exerciseList } = this.props;
+  const infoMap = exerciseList.map(exercise => (
+    <li key={`Info${exercise._id}`}>
+      <ExerciseInfo exercise={exercise} />
+    </li>
+  ));
 
-    const anchorMap = exerciseList.map(exercise => (
-      <li key={`Anchor${exercise._id}`}>
-        <a href={`#${exercise.name.replace(/\s/g, '')}`}>{exercise.name}</a>
-      </li>
-    ));
+  return (
+    <main>
+      <div>
+        <ul>{anchorMap}</ul>
+      </div>
 
-    const infoMap = exerciseList.map(exercise => (
-      <li key={`Info${exercise._id}`}>
-        <ExerciseInfo exercise={exercise} />
-      </li>
-    ));
-
-    return (
-      <main>
-        <div>
-          <ul>{anchorMap}</ul>
-        </div>
-
-        <div>
-          <ul>{infoMap}</ul>
-        </div>
-      </main>
-    );
-  }
-}
+      <div>
+        <ul>{infoMap}</ul>
+      </div>
+    </main>
+  );
+};
 
 export default ExerciseInfoList;
