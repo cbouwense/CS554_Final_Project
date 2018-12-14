76d7f795f7ee9a040599c17dbdc2aa52f6dfa139
Ben Iofel <iofelben+github@gmail.com>
2018-12-13 22:39:41 -0500

    Login (#24)

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {AppRouter} from './router';
+import { AppRouter } from './Router';
 import registerServiceWorker from './registerServiceWorker';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <AppRouter />
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
   , document.getElementById('root'),
 );
 registerServiceWorker();
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,18 +1,14 @@
-import { rootReducer } from '../reducers';
 import { compose, applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import { rootReducer } from '../reducers';
+import initialState from './initialState';
 
-export const initialState = {
-  user: null,
-  exerciseEvents: [],
-};
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(
   rootReducer,
   initialState,
   composeEnhancers(
     applyMiddleware(logger, thunk)
   )
-)
+);
