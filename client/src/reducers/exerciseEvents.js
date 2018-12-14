154c60ecb9afc43a82de986420072abf78798ce5
Taber McFarlin <tabermcfarlin@gmail.com>
2018-12-14 16:44:04 -0500

    Exercise Event Delete button

--- client/src/reducers/exerciseEvents.js
+++ client/src/reducers/exerciseEvents.js
@@ -1,13 +1,19 @@
-import initialState from "../store/initialState";
-import { EXERCISE_EVENT_CREATED, USER_LOGIN_SUCCESS } from "../actions";
+import initialState from '../store/initialState';
+import {
+  EXERCISE_EVENT_CREATED,
+  USER_LOGIN_SUCCESS,
+  EXERCISE_EVENT_DELETED
+} from '../actions';
 
 export default function exerciseEventsReducer(state = initialState.exerciseEvents, action) {
   switch (action.type) {
     case EXERCISE_EVENT_CREATED:
-      return [...state, action.data]
+      return [...state, action.data];
+    case EXERCISE_EVENT_DELETED:
+      return state.filter(exerciseEvent =>  exerciseEvent._id !== action.data._id );
     case USER_LOGIN_SUCCESS:
-      return action.data.exerciseEvents
+      return action.data.exerciseEvents;
     default:
-      return state
+      return state;
   }
 }
