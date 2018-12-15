2c1f1573e07afe252bba91a80b05b475e4090a1d
Matt Rota <mrota@stevens.edu>
2018-12-15 03:57:50 -0500

    removed commented code, console logs

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -44,15 +44,7 @@ export function registerUser(username, password) {
 export function updateUser(userId, data) {
   return async dispatch => {
     try {
-      console.log(data);
       const res = await axios.patch(`http://localhost:4000/api/user/${userId}`, data);
-      // const res = await fetch(
-      //   `http://localhost:4000/api/user/${userId}`,
-      //   {
-      //     method: 'PATCH',
-      //     body: data
-      //   }
-      // );
       return dispatch({
         type: USER_LOGIN_SUCCESS,
         data: res.data
